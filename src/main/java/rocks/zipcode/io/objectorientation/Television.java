dd142d956d09e4d5f63e3b578ae2a29a324cbd02
Nirmala <nirmala.talari@gmail.com>
2019-04-16 12:49:55 -0400

    ObjectOrientation

--- src/main/java/rocks/zipcode/io/objectorientation/Television.java
+++ src/main/java/rocks/zipcode/io/objectorientation/Television.java
@@ -1,13 +1,29 @@
 package rocks.zipcode.io.objectorientation;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 public class Television {
+    Boolean onTv;
+  //  Television television;
+    TVChannel channel;
+
+    public Television() {
+        this.onTv = false;
+        this.channel = null;
+    }
+
     public void turnOn() {
+        this.onTv = true;
     }
 
     public void setChannel(Integer channel) {
+        if (this.onTv) {
+            this.channel = TVChannel.getByOrdinal(channel);
+        }
+        else throw new IllegalStateException();
     }
 
     public TVChannel getChannel() {
-        return null;
+        return this.channel;
     }
 }
