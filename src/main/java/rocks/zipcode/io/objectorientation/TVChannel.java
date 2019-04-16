bd33570ee4bc4cfb7d07d3a7cf9216b3920114dc
Nirmala <nirmala.talari@gmail.com>
2019-04-16 12:35:31 -0400

    TVChannelWIP

--- src/main/java/rocks/zipcode/io/objectorientation/TVChannel.java
+++ src/main/java/rocks/zipcode/io/objectorientation/TVChannel.java
@@ -4,14 +4,31 @@ import java.util.Arrays;
 import java.util.function.Predicate;
 
 public enum TVChannel {
-    ADD_ENUMERATIONS_HERE;
+    MSN(0,"MSN"),
+    CNN(1,"CNN"),
+    FOX(2,"FOX"),
+    AE(3,"AE"),
+    MTV(4,"MTV"),
+    BET(5,"BET"),
+    HBO(6,"HBO"),
+    NBC(7,"NBC"),
+    CBS(8,"CBS");
+
+    private int channelNumber;
+    private String channelName;
+
+    TVChannel(int number, String name) {
+        this.channelNumber = number;
+        this.channelName = name;
+    }
 
     /**
      * @param ordinal; ordinal of the expected enumeration
      * @return enumeration with respective ordinal
      */
     public static TVChannel getByOrdinal(Integer ordinal) {
-        return null;
+        TVChannel[] arr =  TVChannel.values();
+        return arr[ordinal];
     }
 
     /**
@@ -19,6 +36,7 @@ public enum TVChannel {
      * @return enumeration with respective name
      */
     public static TVChannel getByName(String name) {
+
         return find(channel -> name.equals(channel.name()));
     }
 
