d7ed4918089372ba296ad8d80f05678de5456c3c
Nirmala <nirmala.talari@gmail.com>
2019-04-16 12:09:05 -0400

    PowerSet

--- src/main/java/rocks/zipcode/io/collections/PowerSet.java
+++ src/main/java/rocks/zipcode/io/collections/PowerSet.java
@@ -1,16 +1,15 @@
 package rocks.zipcode.io.collections;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author leon on 10/01/2019.
  */
 public class PowerSet<TypeOfSet> {
-    protected final Set<TypeOfSet> originalSet;
+    protected Set<TypeOfSet> originalSet;
 
     public PowerSet(TypeOfSet[] originalSet) {
+
         this.originalSet = new HashSet<>(Arrays.asList(originalSet));
     }
 
@@ -18,13 +17,31 @@ public class PowerSet<TypeOfSet> {
      * @return the powerset of `originalSet`
      */
     public Set<Set<TypeOfSet>> permute() {
-        return null;
+
+        Set<Set<TypeOfSet>> sets = new LinkedHashSet<>();
+        if (originalSet.isEmpty()) {
+            sets.add(new HashSet<>());
+            return sets;
+        }
+        List<TypeOfSet> list = new ArrayList<>(originalSet);
+        TypeOfSet head = list.get(0);
+        Set<TypeOfSet> rest = new LinkedHashSet<>(list.subList(1, list.size()));
+        this.originalSet = rest;
+        for (Iterable<TypeOfSet> set : permute()) {
+            Set<TypeOfSet> newSet = new LinkedHashSet<>();
+            newSet.add(head);
+            newSet.addAll((Collection<? extends TypeOfSet>) set);
+            sets.add(newSet);
+            sets.add((Set<TypeOfSet>) set);
+        }
+        return sets;
     }
 
     /**
      * @return `originalSet`
      */
     public Set<TypeOfSet> getOriginalSet() {
+
         return originalSet;
     }
 }
